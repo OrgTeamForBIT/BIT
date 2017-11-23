39abb1474dd7d691acb672a0276ebd0f8a6bea72
halfmoon2014 <mrzhangmh@gmail.com>
2017-11-23 10:24:30 +0800

    init

---
+++ Bitfinex.Net/Implementations/Request.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Bitfinex.Net.Interfaces;
+
+namespace Bitfinex.Net.Implementations
+{
+    public class Request : IRequest
+    {
+        private readonly WebRequest request;
+
+        public Request(WebRequest request)
+        {
+            this.request = request;
+        }
+
+        public WebHeaderCollection Headers
+        {
+            set
+            {
+                request.Headers = value;
+            }
+            get
+            {
+                return request.Headers;
+            }
+            //get => request.Headers;
+            //set => request.Headers = value;
+        }
+
+        public string ContentType
+        {
+            set
+            {
+                request.ContentType = value;
+            }
+            get
+            {
+                return request.ContentType;
+            }
+            //get => request.ContentType;
+            //set => request.ContentType = value;
+        }
+
+        public string Accept
+        {
+            set
+            {
+                ((HttpWebRequest)request).Accept = value;
+            }
+            get
+            {
+                return ((HttpWebRequest)request).Accept;
+            }
+            //get => ((HttpWebRequest) request).Accept;
+            //set => ((HttpWebRequest) request).Accept = value;
+        }
+
+        public long ContentLength
+        {
+            set
+            {
+                ((HttpWebRequest)request).ContentLength = value;
+            }
+            get
+            {
+                return ((HttpWebRequest)request).ContentLength;
+            }
+            //get => ((HttpWebRequest)request).ContentLength;
+            //set => ((HttpWebRequest)request).ContentLength = value;
+        }
+
+        public string Method
+        {
+            set
+            {
+                request.Method = value;
+            }
+            get
+            {
+                return request.Method;
+            }
+            //get => request.Method;
+            //set => request.Method = value;
+        }
+
+        public Uri RequestUri => request.RequestUri;
+
+        public Stream GetRequestStream()
+        {
+            return request.GetRequestStream();
+        }
+
+        public IResponse GetResponse()
+        {
+            return new Response(request.GetResponse());
+        }
+    }
+}
---
+++ WF/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Bitfinex.Net;
+using Bitfinex.Net.Objects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread t = new Thread(start);
+            //t.Start();
+            Task<string> task = new Task<string>(() => start());
+            task.Start();            
+            this.richTextBox1.Text = task.Result;            
+        }
+        //static Task<int> CreateTask(string name)
+        //{
+        //    return new Task<int>(() => TaskMethod(name));
+        //}
+
+        public string start()
+        {
+            string info = "";
+            BitfinexClient bit = new BitfinexClient();
+            bit.SetApiKey("cndYoNyd8Tp2g1rAslKFPCcuxYklO3PEprVrlTYMT5l");
+            bit.SetApiSecret("ms0cM9Hd1FgEkR6p9TZYVWfw9yDiO3OXg7o2mMhvvx8");
+            BitfinexApiResult<BitfinexAccountInfo> result = bit.GetAccountInfo();
+            if (result.Error == null)
+            {
+                info += "MakerFees:" + result.Result.MakerFees;
+                info += "TakerFees:" + result.Result.TakerFees;
+                int i = 1;
+                foreach(BitfinexFee bf in result.Result.Fees)
+                {
+                    info += "BitfinexFee:"+i.ToString();
+                    info += "MakerFees:" + bf.MakerFees;
+                    info += "Pairs:" + bf.Pairs;
+                    info += "TakerFees:" + bf.TakerFees;
+                    info += "\r\n";
+                    i++;
+                }               
+            }
+            else
+            {
+                info = "error:" + result.Error.ErrorCode.ToString() + result.Error.ErrorMessage;
+            }
+            return info;            
+        }
+    }
+}
